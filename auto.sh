778cb465577b56b98ba694e2e4eee76020db9cd7
badboy4471 <badboy4471@sina.com>
2015-11-06 17:32:19 +0800

    添加 服务端db配置，和修改部署代码

---
+++ auto.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+LANG=en_US.UTF-8
+
+function deploy(){
+    duanqu_dir=$1
+    tomcat_dir=$2
+    cd ${tomcat_dir}
+
+    echo '保存当前日志信息-------'
+    cp logs/catalina.out  data/catalina.out.$(date +%Y%m%d%H%M%S)
+    rm logs/*
+
+    echo '删除 apps '
+    rm webapps/* -r
+
+    echo '删除work下的缓存'
+    rm work/Catalina/localhost/* -r
+
+    echo '部署--------------'
+
+    cp ${duanqu_dir}/art-web/target/art.war webapps/art.war
+}
+
+echo 'repo update----------------'
+git checkout master
+git pull origin master
+
+
+echo 'rename database config'
+
+cp /mnt/code/wanghh-art/art-dao/src/main/resources/online-dataSource.properties /mnt/code/wanghh-art/art-dao/src/main/resources/dataSource.properties
+
+
+echo 'mvn clean ----------------'
+
+mvn -Dmaven.test.skip clean package
+
+echo 'kill tomcat---------------'
+ps aux | grep tomcat | awk '{print $2}' | xargs kill -9
+
+deploy `pwd` /mnt/tomcat
+
+echo 'start tomcat--------------'
+
+bin/startup.sh
