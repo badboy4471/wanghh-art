46bf965286b7613d9594c06597f2541e5c6ce12c
badboy4471 <badboy4471@sina.com>
2015-11-15 11:47:12 +0800

    Login 跳转

--- art-web/src/main/java/com/whh/art/web/controller/LoginInterceptor.java
+++ art-web/src/main/java/com/whh/art/web/controller/LoginInterceptor.java
@@ -42,7 +42,7 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 				builder.append("<script type=\"text/javascript\" charset=\"UTF-8\">");
 				builder.append("alert(\"页面过期，请重新登录\");");
 				builder.append("window.top.location.href=\"");
-//				builder.append(request.getContextPath());
+				builder.append(request.getContextPath());
 				builder.append("/login.html\";</script>");
 				out.print(builder.toString());
 				out.close();
